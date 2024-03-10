b4763fc58d277b44c8a2ef7aa74fbbea1c1c1da7
darrengalvin <149120692+darrengalvin@users.noreply.github.com>
2024-03-10 04:19:08 +0000

    Remove all files

--- firebase/custom_cloud_functions/index.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin/app");
-admin.initializeApp();
-
-const generateCloudinarySignature = require("./generate_cloudinary_signature.js");
-exports.generateCloudinarySignature =
-  generateCloudinarySignature.generateCloudinarySignature;
-const cleanTheJsonCloud = require("./clean_the_json_cloud.js");
-exports.cleanTheJsonCloud = cleanTheJsonCloud.cleanTheJsonCloud;
